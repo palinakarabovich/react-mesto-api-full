857c288b76e7db4297bddd495111a2765cd64f0d
palinakarabovich <98895630+palinakarabovich@users.noreply.github.com>
2022-10-11 21:34:02 +0200

    baseUrl changed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -111,7 +111,7 @@ class Api {
 }
 
 const api = new Api({
-    baseUrl: 'http://localhost:3002',
+    baseUrl: 'http://apipalinakarabovich.mesto.nomoredomains.icu/',
   });
 
   export default api;
